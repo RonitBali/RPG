e15a0fc40399eb9bf9f0e93801e858803ef7aed3
RonitBali <balironit@gmail.com>
2024-06-22 02:51:27 +0530

    commit

---
+++ rpg/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Test</h1>
+      <h2 className='text-white'>test</h2>
+    </>
+  )
+}
+
+export default App
